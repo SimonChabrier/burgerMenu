cab7bbd1ba07d7b41d7781afd26e864a060c30d2
SimonChabrier <simonchabrier@gmail.com>
2022-10-01 19:27:36 +0200

    fist commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## JS Side Bar Burger Menu
+
+Simple js css and html side bar burger menu ready to use.
---
+++ js/menu.js
@@ -0,0 +1,33 @@
+const menu = {
+
+    init:function(){
+        console.log('appInit');
+        menu.addAllEventLIsteners();
+    },
+
+    addAllEventLIsteners:function(){
+        console.log('add all listeners')
+
+        document.getElementById('openBtn').addEventListener('click', (event => {
+            menu.handleOpenNav()
+        }));
+
+        document.getElementById('closeBtn').addEventListener('click', (event => {
+            menu.handleCloseNav()
+        }));
+    },
+
+    handleOpenNav: function() {
+        let sidenav = document.getElementById("idSidenav");
+        sidenav.classList.add("active");
+    },
+
+    handleCloseNav: function() {
+        let sidenav = document.getElementById("idSidenav");
+        sidenav.classList.remove("active");
+    },
+
+}
+
+
+document.addEventListener("DOMContentLoaded", menu.init);
